82b4481b89a0af6bc55cef16df0a511318293b40
Liam Welsh <liamwelsh17@gmail.com>
2024-02-25 23:05:38 -0400

    wip

---
+++ lib/shared/presentation/components/box_shadow_image.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class BoxShadowImage extends StatefulWidget {
+  final Image image;
+  final void Function()? onTap;
+  final String text;
+  const BoxShadowImage({super.key, required this.image, this.onTap, required this.text});
+
+  @override
+  State<BoxShadowImage> createState() => _BoxShadowImageState();
+}
+
+class _BoxShadowImageState extends State<BoxShadowImage> {
+  var _isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      onTapDown: (_) => setState(() => _isPressed = true),
+      onTapUp: (_) => setState(() => _isPressed = false),
+      onTapCancel: () => setState(() => _isPressed = false),
+      child: Container(
+        height: widget.image.height,
+        width: widget.image.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: widget.image.image,
+            fit: BoxFit.cover,
+          ),
+          color: Colors.black,
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+          border: Border.all(
+            color: Colors.black
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: _isPressed ? Colors.black : Colors.black.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            widget.text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                color: Colors.white,
+                fontSize: 50
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ test/box_shadow_image_test.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mythos_manager/shared/presentation/components/box_shadow_image.dart';
+
+
+void main() {
+}
