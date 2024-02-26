41a71fee42de2b0fc0680026351b834b7050f833
Jonathon Meney <jonmeney@gmail.com>
2024-02-26 18:03:18 -0400

    added provider scope to root app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mythos_manager/features/home/presentation/screens/home_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mythos_manager/features/home/presentation/screens/home_screen.dart';
 import 'package:mythos_manager/shared/presentation/components/mythos_theme_data.dart';
 import 'firebase_options.dart';
 
@@ -12,7 +13,7 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mythos Manager',
       theme: mythosThemeData,
-      home: const HomePage(),
+      home: const HomeScreen(),
     );
   }
 }
