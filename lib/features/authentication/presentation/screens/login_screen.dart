7bd770111cc79453668874e6556292a52b1bf656
Jonathon Meney <jonmeney@gmail.com>
2024-02-26 18:04:15 -0400

    implemented login and sign up screens

--- lib/features/authentication/presentation/screens/login_screen.dart
+++ lib/features/authentication/presentation/screens/login_screen.dart
@@ -1,1 +1,67 @@
-class LoginScreen {}
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class LoginScreen extends HookConsumerWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final TextEditingController emailTextController =
+        useTextEditingController();
+    final TextEditingController passwordTextController =
+        useTextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+                controller: emailTextController,
+                decoration: const InputDecoration(labelText: "Email"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+                controller: passwordTextController,
+                decoration: const InputDecoration(labelText: "Password"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    // TODO: wire together to auth controller
+                  }),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Don't have an account?\nSign Up",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  onPressed: () {
+                    // TODO: send to sign up route
+                    // Navigator.pushReplacementNamed(context, 'routeName');
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/authentication/presentation/screens/sign_up_screen.dart
+++ lib/features/authentication/presentation/screens/sign_up_screen.dart
@@ -1,1 +1,76 @@
-class SignUpScreen {}
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SignUpScreen extends HookConsumerWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final TextEditingController usernameTextController =
+    useTextEditingController();
+    final TextEditingController emailTextController =
+    useTextEditingController();
+    final TextEditingController passwordTextController =
+    useTextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+                controller: usernameTextController,
+                decoration: const InputDecoration(labelText: "Username"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+                controller: emailTextController,
+                decoration: const InputDecoration(labelText: "Email"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+                controller: passwordTextController,
+                decoration: const InputDecoration(labelText: "Password"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                  child: const Text('Sign Up'),
+                  onPressed: () {
+                    // TODO: wire together to auth controller
+                  }),
+            ),
+            Container(
+              margin: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Already have an account?\nLogin",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  onPressed: () {
+                    // TODO: send to login route
+                    // Navigator.pushReplacementNamed(context, 'routeName');
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
