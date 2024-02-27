c533e73b26807c2a21d4e55fe8b503e8e81410c7
Liam Welsh <liamwelsh17@gmail.com>
2024-02-27 01:41:22 -0400

    Ensure row elements have more height then column elements when possible in home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mythos_manager/shared/presentation/components/box_shadow_image.dart';
 
-const placeHolderImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/640px-Donald_Trump_official_portrait.jpg";
+const placeHolderImage =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/640px-Donald_Trump_official_portrait.jpg";
 
 /// Home screen.
 ///
@@ -9,9 +10,7 @@ const placeHolderImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/5
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  final cardTextStyle = const TextStyle(
-    color: Colors.white
-  );
+  final cardTextStyle = const TextStyle(color: Colors.white);
 
   @override
   Widget build(BuildContext context) {
@@ -22,75 +21,87 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Container(
         margin: const EdgeInsets.all(35),
-
         child: Column(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: BoxShadowImage(
-                      image: Image.network(placeHolderImage, height: 200),
-                      text: Text(
-                          "Campaigns",
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: BoxShadowImage(
+                  image: Image.network(
+                    placeHolderImage,
+                    height: 200,
+                  ),
+                  text: Text(
+                    "Campaigns",
+                    textAlign: TextAlign.center,
+                    style: cardTextStyle,
+                  ),
+                  onTap: () {}, // TODO onTap Handler for navigation
+                ),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: BoxShadowImage(
+                        image: Image.network(
+                          placeHolderImage,
+                          height: 300,
+                        ),
+                        text: Text(
+                          "Favourite\nCharacter",
                           textAlign: TextAlign.center,
                           style: cardTextStyle,
-                      ),
-                      onTap: () {}, // TODO onTap Handler for navigation
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        child: BoxShadowImage(
-                            image: Image.network(placeHolderImage, height: 300,),
-                            text: Text(
-                                "Favourite\nCharacter",
-                                textAlign: TextAlign.center,
-                                style: cardTextStyle,
-                            ),
-                            onTap: () {}, // TODO onTap Handler for navigation
                         ),
+                        onTap: () {}, // TODO onTap Handler for navigation
                       ),
                     ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        child: BoxShadowImage(
-                            image: Image.network(placeHolderImage, height: 300,),
-                            text: Text(
-                                "Current\nCampaign",
-                                textAlign: TextAlign.center,
-                                style: cardTextStyle,
-                            ),
-                            onTap: () {}, // TODO onTap Handler for navigation
-
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: BoxShadowImage(
+                        image: Image.network(
+                          placeHolderImage,
+                          height: 300,
                         ),
+                        text: Text(
+                          "Current\nCampaign",
+                          textAlign: TextAlign.center,
+                          style: cardTextStyle,
+                        ),
+                        onTap: () {}, // TODO onTap Handler for navigation
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: BoxShadowImage(
-                      image: Image.network(placeHolderImage),
-                      text: Text(
-                        "Character Creator",
-                        textAlign: TextAlign.center,
-                        style: cardTextStyle,
-                      ),
-                      onTap: () {}, // TODO onTap Handler for navigation
                   ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: BoxShadowImage(
+                  image: Image.network(
+                    placeHolderImage,
+                    height: 200,
+                  ),
+                  text: Text(
+                    "Character Creator",
+                    textAlign: TextAlign.center,
+                    style: cardTextStyle,
+                  ),
+                  onTap: () {}, // TODO onTap Handler for navigation
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
     );
   }
