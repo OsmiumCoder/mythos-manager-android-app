dd903db51a0b6757276d92ec60223513b14c6922
Jonathon Meney <jonmeney@gmail.com>
2024-02-26 21:58:00 -0400

    changed home screen import to screen export file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mythos_manager/features/home/presentation/screens/home_screen.dart';
+import 'package:mythos_manager/features/home/presentation/screens/screens.dart';
 import 'package:mythos_manager/shared/presentation/themes/theme_data.dart';
 import 'firebase_options.dart';
 
