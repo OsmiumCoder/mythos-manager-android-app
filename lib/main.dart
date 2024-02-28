8108c51c0ca12b849d74424e8c0b4146c3430b0d
Jonathon Meney <jonmeney@gmail.com>
2024-02-27 22:18:59 -0400

    revert on main to match main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mythos_manager/features/authentication/presentation/screens/screens.dart';
 import 'package:mythos_manager/features/home/presentation/screens/screens.dart';
 import 'package:mythos_manager/shared/presentation/themes/theme_data.dart';
 import 'firebase_options.dart';
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mythos Manager',
       theme: themeData,
-      home: const SignUpScreen(),
+      home: const HomeScreen(),
     );
   }
 }
