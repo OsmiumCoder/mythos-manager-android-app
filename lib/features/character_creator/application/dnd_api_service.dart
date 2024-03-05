fcce90b8b3883c22ad5a667fe800cc9e8f4679eb
Jonathon Meney <jonmeney@gmail.com>
2024-03-05 02:33:10 -0400

    implemented all needed methods at service level, tests included

---
+++ lib/features/character_creator/application/dnd_api_service.dart
@@ -0,0 +1,69 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mythos_manager/features/character_creator/data/dnd_api_repository.dart';
+
+/// Provides a [DNDAPIService].
+final dndApiService = Provider<DNDAPIService>((ref) {
+  return DNDAPIService(ref.watch(dndApiRepository));
+});
+
+/// Responsible for communication between controller and repository.
+///
+/// Author: Jonathon Meney
+class DNDAPIService {
+
+  /// The [DNDAPIRepository] used to fetch API data.
+  final DNDAPIRepository _repository;
+
+  /// Constructs a [DNDAPIService].
+  DNDAPIService(this._repository);
+
+  /// Returns all playable races.
+  Future<Map<String, dynamic>> getAllRaces() async {
+    return await _repository.getAllRaces();
+  }
+
+  /// Returns specific information for a given race.
+  Future<Map<String, dynamic>> getRace(String race) async {
+    return await _repository.getRace(race);
+  }
+
+  /// Returns specific information for a given subrace.
+  Future<Map<String, dynamic>> getSubrace(String subrace) async {
+    return await _repository.getSubrace(subrace);
+  }
+
+  /// Returns all playable classes.
+  Future<Map<String, dynamic>> getAllClasses() async {
+    return await _repository.getAllClasses();
+  }
+
+  /// Returns specific information about a given class.
+  Future<Map<String, dynamic>> getClass(String className) async {
+    return await _repository.getClass(className);
+  }
+
+  /// Returns specific information about a given subclass.
+  Future<Map<String, dynamic>> getSubclass(String subclass) async {
+    return await _repository.getSubclass(subclass);
+  }
+
+  /// Returns all playable backgrounds.
+  Future<Map<String, dynamic>> getAllBackgrounds() async {
+    return await _repository.getAllBackgrounds();
+  }
+
+  /// Returns specific information about a given background.
+  Future<Map<String, dynamic>> getBackground(String background) async {
+    return await _repository.getBackground(background);
+  }
+
+  /// Returns all languages.
+  Future<Map<String, dynamic>> getAllLanguages() async {
+    return await _repository.getAllLanguages();
+  }
+
+  /// Returns the list of spells for a specific class.
+  Future<Map<String, dynamic>> getSpellsForClass(String className) async {
+    return await _repository.getSpellsForClass(className);
+  }
+}
