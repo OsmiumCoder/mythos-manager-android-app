88997748842385b618544b5423aefc27f498e95f
Jonathon Meney <jonmeney@gmail.com>
2024-03-07 12:22:58 -0400

    character map to track what need to be stored, to be extended, only holds racial data currently

---
+++ lib/features/character_creator/domain/character_map.dart
@@ -0,0 +1,29 @@
+
+/// This map is the default map for a new character.
+///
+/// As a character is de
+final Map<String, dynamic> defaultCharacterDataMap = {
+  "race": "",
+  "subrace": "",
+  "size": "",
+  "speed": 0,
+  "languages": [],
+  "ability_scores": {
+    "str": 6,
+    "dex": 6,
+    "con": 6,
+    "int": 6,
+    "wis": 6,
+    "cha": 6,
+  },
+  "ability_score_increases": {
+    "str": 0,
+    "dex": 0,
+    "con": 0,
+    "int": 0,
+    "wis": 0,
+    "cha": 0,
+  },
+  "traits": [],
+  "proficiencies": []
+};
