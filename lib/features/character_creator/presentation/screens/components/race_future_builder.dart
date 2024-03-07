7791874e8666e96eef754a7daf8b5c0d81ebf901
Jonathon Meney <jonmeney@gmail.com>
2024-03-06 22:43:30 -0400

    moved future builders into separate components

---
+++ lib/features/character_creator/presentation/screens/components/race_future_builder.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mythos_manager/features/character_creator/presentation/controllers/dnd_api_controller.dart';
+import 'package:mythos_manager/features/character_creator/presentation/screens/components/components.dart';
+
+class RaceFutureBuilder extends HookConsumerWidget {
+  const RaceFutureBuilder({
+    super.key,
+    required this.raceController,
+    required this.textStyle,
+    required this.subraceController,
+  });
+
+  final TextEditingController raceController;
+  final TextStyle textStyle;
+  final TextEditingController subraceController;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FutureBuilder(
+        future: ref
+            .watch(dndApiController)
+            .getRace(raceController.text.toLowerCase()),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final race = snapshot.data!;
+            final String abilityBonuses =
+                race["ability_bonuses"].map((element) {
+              return "+${element["bonus"]} ${element["ability_score"]["name"]}";
+            }).join(", ");
+
+            final String languages = race["languages"].map((element) {
+              return "${element["name"]}";
+            }).join(", ");
+
+            final String traits = race["traits"].map((element) {
+              return "${element["name"]}";
+            }).join(", ");
+
+            final String startingProficiencies =
+                race["starting_proficiencies"].map((element) {
+              return "${element["name"]}";
+            }).join(", ");
+
+            final List startingProficienciesOptions =
+                race["starting_proficiency_options"]?["from"]["options"] ?? [];
+
+            final List subraces = race["subraces"];
+
+            return Column(
+              children: [
+                // Race Selection.
+                Text(
+                  "Alignment: ${race["Alignment"]}",
+                  style: textStyle,
+                ),
+                Text(
+                  "Size: ${race["size"]}",
+                  style: textStyle,
+                ),
+                Text(
+                  "Languages: $languages",
+                  style: textStyle,
+                ),
+                Text(
+                  "Speed: ${race["speed"]}",
+                  style: textStyle,
+                ),
+                Text(
+                  "Ability Score Increases: $abilityBonuses",
+                  style: textStyle,
+                ),
+                traits.isNotEmpty
+                    ? Text(
+                        "Traits: $traits",
+                        style: textStyle,
+                      )
+                    : const SizedBox.shrink(),
+                startingProficiencies.isNotEmpty
+                    ? Text("Starting Proficiencies: $startingProficiencies")
+                    : const SizedBox.shrink(),
+                startingProficienciesOptions.isNotEmpty
+                    ? Text(
+                        "Starting Proficiencies Options",
+                        style: textStyle,
+                      )
+                    : const SizedBox.shrink(),
+                startingProficienciesOptions.isNotEmpty
+                    ? Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: DropdownMenu(
+                            dropdownMenuEntries:
+                                startingProficienciesOptions.map((option) {
+                          return DropdownMenuEntry(
+                              value: option["item"]["index"],
+                              label: option["item"]["name"]);
+                        }).toList()),
+                      )
+                    : const SizedBox.shrink(),
+                subraces.isNotEmpty
+                    ? Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: DropdownMenu(
+                            onSelected: (selected) {
+                              subraceController.text = selected;
+                            },
+                            dropdownMenuEntries: subraces.map((subrace) {
+                              return DropdownMenuEntry(
+                                  value: subrace["index"],
+                                  label: subrace["name"]);
+                            }).toList()),
+                      )
+                    : const SizedBox.shrink(),
+
+                // Subrace selection.
+                subraceController.text.isNotEmpty
+                    ? SubraceFutureBuilder(
+                        subraceController: subraceController,
+                        textStyle: textStyle)
+                    : const SizedBox.shrink(),
+              ],
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
+  }
+}
---
+++ lib/features/character_creator/presentation/screens/components/subrace_future_builder.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mythos_manager/features/character_creator/presentation/controllers/dnd_api_controller.dart';
+
+class SubraceFutureBuilder extends HookConsumerWidget {
+  const SubraceFutureBuilder({
+    super.key,
+    required this.subraceController,
+    required this.textStyle,
+  });
+
+  final TextEditingController subraceController;
+  final TextStyle textStyle;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FutureBuilder(
+        future: ref
+            .watch(dndApiController)
+            .getSubrace(subraceController.text.toLowerCase()),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final subrace = snapshot.data!;
+
+            final String abilityBonuses =
+                subrace["ability_bonuses"].map((element) {
+              return "+${element["bonus"]} ${element["ability_score"]["name"]}";
+            }).join(", ");
+
+            final String startingProficiencies =
+                subrace["starting_proficiencies"].map((element) {
+              return "${element["name"]}";
+            }).join(", ");
+
+            final String languages = subrace["languages"].map((element) {
+              return "${element["name"]}";
+            }).join(", ");
+
+            final String racialTraits = subrace["racial_traits"].map((element) {
+              return "${element["name"]}";
+            }).join(", ");
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Ability Score Increases: $abilityBonuses",
+                  style: textStyle,
+                ),
+                Text(
+                  "Starting Proficiencies: $startingProficiencies",
+                  style: textStyle,
+                ),
+                Text(
+                  "Languages: $languages",
+                  style: textStyle,
+                ),
+                Text(
+                  "Traits: $racialTraits",
+                  style: textStyle,
+                ),
+              ],
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
+  }
+}
