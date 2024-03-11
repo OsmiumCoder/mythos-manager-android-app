83dde628712926204bf0c5045abc96eb36986dbf
Jonathon Meney <jonmeney@gmail.com>
2024-03-10 23:06:26 -0300

    lint

--- test/features/characters/application/character_service_test.dart
+++ test/features/characters/application/character_service_test.dart
@@ -16,38 +16,6 @@ class MockCharacterRepository extends Mock implements CharacterRepository {}
 
 void main() {
   group("CharacterService tests", () {
-    Character character = Character(
-      skillProficiencies: ["perception", "arcana"],
-      equipmentProficiencies: ["sword", "armor"],
-      equipment: ["longsword", "leather armor"],
-      race: "Elf",
-      subrace: "High Elf",
-      size: "Medium",
-      speed: 30,
-      abilityScoreIncreases: {"str": 2, "dex": 1},
-      racialTraits: ["Dark vision", "Resistance"],
-      className: "Wizard",
-      subclass: "Evocation",
-      hitDie: 12,
-      savingThrows: ["wis", "int"],
-      abilityScores: {
-        "str": 18,
-        "dex": 10,
-        "con": 14,
-        "int": 12,
-        "wis": 16,
-        "cha": 9
-      },
-      background: "Acolyte",
-      backgroundFeatureName: "Feature Name",
-      backgroundFeatureDesc: "A short description.",
-      alignment: "Lawful Good",
-      age: "300 years",
-      weight: "200 lbs.",
-      height: "6 feet",
-      backstory: "A tragic story with parents being murdered.",
-    );
-
     late AuthenticationRepository mockAuthRepo;
     late MockUser mockUser;
     late CharacterRepository mockCharacterRepository;
