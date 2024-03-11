e8e0a61894f999bc1d7d7190880d75141029d8af
Jonathon Meney <jonmeney@gmail.com>
2024-03-10 23:13:16 -0300

    finished controller and tests

---
+++ lib/features/characters/presentation/controllers/character_controller.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mythos_manager/features/character_creator/domain/character.dart';
+import 'package:mythos_manager/features/characters/application/character_service.dart';
+
+/// Provides a [CharacterController].
+final characterControllerProvider = AsyncNotifierProvider<CharacterController, List<Character>>((){
+  return CharacterController();
+});
+
+/// Delegates requests related to [Character]s.
+///
+/// Author: Jonathon Meney
+class CharacterController extends AsyncNotifier<List<Character>> {
+
+  /// Returns a list of the signed in users [Character]s.
+  Future<List<Character>> _fetchCharacters() async {
+    return ref.watch(characterServiceProvider).fetchCharacters();
+  }
+
+  @override
+  FutureOr<List<Character>> build() {
+    // Fetch characters on build.
+    return _fetchCharacters();
+  }
+}
