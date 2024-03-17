19f9a24bd5af82064e73579c1f0ea36df5885ebb
Shreif Abdalla <sabdalla11124@upei.ca>
2024-03-17 13:38:32 -0300

    Completed Class Selection Screen page

--- lib/features/character_creator/presentation/screens/components/class_selection/class_future_builder.dart
+++ lib/features/character_creator/presentation/screens/components/class_selection/class_future_builder.dart
@@ -3,6 +3,9 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mythos_manager/features/character_creator/presentation/controllers/dnd_api_controller.dart';
 
+
+
+///Author: Shreif Abdalla
 class ClassFutureBuilder extends HookConsumerWidget {
   const ClassFutureBuilder({
     super.key,
