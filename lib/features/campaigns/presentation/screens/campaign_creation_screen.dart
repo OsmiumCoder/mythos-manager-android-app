96eb5cce826af0de6c11afc9eeec02793681976a
Liam Welsh <liamwelsh17@gmail.com>
2024-03-18 23:05:41 -0300

    Refactor: CampaignCreationScreen extends HookWidget

--- lib/features/campaigns/presentation/screens/campaign_creation_screen.dart
+++ lib/features/campaigns/presentation/screens/campaign_creation_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CampaignCreationScreen extends StatelessWidget {
+class CampaignCreationScreen extends HookWidget {
   const CampaignCreationScreen({super.key});
 
   @override
