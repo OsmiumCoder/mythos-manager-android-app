60b48c6d2ab8db58078a37deacab88b5a833d246
Jonathon Meney <jonmeney@gmail.com>
2024-03-23 17:32:53 -0300

    finished main display page for a character

--- lib/features/characters/presentation/screens/components/character_display/main_character_display.dart
+++ lib/features/characters/presentation/screens/components/character_display/main_character_display.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mythos_manager/features/characters/domain/character.dart';
+import 'package:mythos_manager/features/characters/presentation/screens/components/character_display/ability_score_row.dart';
+import 'package:mythos_manager/features/characters/presentation/screens/components/character_display/equipment_card.dart';
+import 'package:mythos_manager/features/characters/presentation/screens/components/character_display/features_list.dart';
+import 'package:mythos_manager/features/characters/presentation/screens/components/character_display/proficiencies_card.dart';
+import 'package:mythos_manager/features/characters/presentation/screens/components/character_display/racial_trait_card.dart';
 import 'package:mythos_manager/shared/extensions/capitalize.dart';
 
 class MainCharacterDisplay extends ConsumerWidget {
@@ -11,294 +16,83 @@ class MainCharacterDisplay extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                  child: Text(
-                character.className!,
-                style: const TextStyle(fontSize: 20),
-              )),
-              Container(
-                margin: const EdgeInsets.only(bottom: 10),
-                child: Center(
-                  child: Text(
-                    "${character.race!.capitalize()}, ${character.subrace != null ? "${character.subrace!.capitalize()}, " : ""}${character.size}",
-                    style: const TextStyle(fontSize: 14),
-                  ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+                child: Text(
+              character.className!,
+              style: const TextStyle(fontSize: 20),
+            )),
+            Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              child: Center(
+                child: Text(
+                  "${character.race!.capitalize()}, ${character.subrace != null ? "${character.subrace!.capitalize()}, " : ""}${character.size}",
+                  style: const TextStyle(fontSize: 16),
                 ),
               ),
-              AbilityScoreRowComponent(
-                  abilityScores: character.abilityScores!,
-                  abilityScoreIncreases: character.abilityScoreIncreases!,
-                  savingThrows: character.savingThrows!),
-              Card(
-                child: Column(
+            ),
+            AbilityScoreRow(
+                abilityScores: character.abilityScores!,
+                abilityScoreIncreases: character.abilityScoreIncreases!,
+                savingThrows: character.savingThrows!),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
                   children: [
-                    const ListTile(
-                      title: Text("Equipment", style: TextStyle(color: Colors.white, fontSize: 15),)
+                    const Text("Hit Die"),
+                    Container(
+                      padding: const EdgeInsets.all(15),
+                      margin: const EdgeInsets.all(2.5),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Color(0xFF90714A),
+                      ),
+                      child: Center(
+                          child: Text(
+                        "d${character.hitDie}",
+                        style: const TextStyle(color: Colors.white),
+                      )),
                     ),
-                    ListView.builder(itemBuilder: (context, index) {
-                      return Text(character.equipment!.elementAt(index));
-                    })
                   ],
                 ),
-              )
-            ],
-          ),
-        ),
-      );
-  }
-}
-
-class AbilityScoreRowComponent extends StatelessWidget {
-  final Map<String, int> abilityScores;
-  final Map<String, int> abilityScoreIncreases;
-  final Set<String> savingThrows;
-
-  const AbilityScoreRowComponent(
-      {super.key,
-      required this.abilityScores,
-      required this.abilityScoreIncreases,
-      required this.savingThrows});
-
-  @override
-  Widget build(BuildContext context) {
-    final strengthTotal =
-        abilityScores["STR"]! + (abilityScoreIncreases["STR"] ?? 0);
-    final dexterityTotal =
-        abilityScores["DEX"]! + (abilityScoreIncreases["DEX"] ?? 0);
-    final constitutionTotal =
-        abilityScores["CON"]! + (abilityScoreIncreases["CON"] ?? 0);
-    final intelligenceTotal =
-        abilityScores["INT"]! + (abilityScoreIncreases["INT"] ?? 0);
-    final wisdomTotal =
-        abilityScores["WIS"]! + (abilityScoreIncreases["WIS"] ?? 0);
-    final charismaTotal =
-        abilityScores["CHA"]! + (abilityScoreIncreases["CHA"] ?? 0);
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
-          children: [
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                savingThrows.contains("STR")
-                    ? const Icon(
-                        Icons.circle,
-                        size: 15,
-                      )
-                    : const Icon(
-                        Icons.circle_outlined,
-                        size: 15,
+                Column(
+                  children: [
+                    const Text("Speed"),
+                    Container(
+                      padding: const EdgeInsets.all(15),
+                      margin: const EdgeInsets.all(2.5),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Color(0xFF90714A),
                       ),
-                const Text("STR"),
+                      child: Center(
+                          child: Text(
+                        "${character.speed} ft.",
+                        style: const TextStyle(color: Colors.white),
+                      )),
+                    ),
+                  ],
+                ),
               ],
             ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.all(2.5),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 3),
-                  shape: BoxShape.circle,
-                  color: Colors.brown,
-                  gradient: const RadialGradient(
-                    colors: [Colors.brown, Colors.black],
-                  )),
-              child: Center(
-                  child: Text(
-                "$strengthTotal",
-                style: const TextStyle(color: Colors.white),
-              )),
+            ProficienciesCard(
+              skillProficiencies: character.skillProficiencies,
+              equipmentProficiencies: character.equipmentProficiencies,
             ),
-            Text("+${(strengthTotal - 10) ~/ 2}")
+            EquipmentCard(equipment: character.equipment),
+            RacialTraitCard(racialTraits: character.racialTraits),
+            FeaturesList(
+              className: character.className!,
+              subclass: character.subclass!,
+            ),
           ],
         ),
-        Column(
-          children: [
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                savingThrows.contains("DEX")
-                    ? const Icon(
-                        Icons.circle,
-                        size: 15,
-                      )
-                    : const Icon(
-                        Icons.circle_outlined,
-                        size: 15,
-                      ),
-                const Text("DEX"),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.all(2.5),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 3),
-                  shape: BoxShape.circle,
-                  color: Colors.brown,
-                  gradient: const RadialGradient(
-                    colors: [Colors.brown, Colors.black],
-                  )),
-              child: Center(
-                  child: Text(
-                "$dexterityTotal",
-                style: const TextStyle(color: Colors.white),
-              )),
-            ),
-            Text("+${(dexterityTotal - 10) ~/ 2}")
-          ],
-        ),
-        Column(
-          children: [
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                savingThrows.contains("CON")
-                    ? const Icon(
-                        Icons.circle,
-                        size: 15,
-                      )
-                    : const Icon(
-                        Icons.circle_outlined,
-                        size: 15,
-                      ),
-                const Text("CON"),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.all(2.5),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 3),
-                  shape: BoxShape.circle,
-                  color: Colors.brown,
-                  gradient: const RadialGradient(
-                    colors: [Colors.brown, Colors.black],
-                  )),
-              child: Center(
-                  child: Text(
-                "$constitutionTotal",
-                style: const TextStyle(color: Colors.white),
-              )),
-            ),
-            Text("+${(constitutionTotal - 10) ~/ 2}")
-          ],
-        ),
-        Column(
-          children: [
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                savingThrows.contains("INT")
-                    ? const Icon(
-                        Icons.circle,
-                        size: 15,
-                      )
-                    : const Icon(
-                        Icons.circle_outlined,
-                        size: 15,
-                      ),
-                const Text("INT"),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.all(2.5),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 3),
-                  shape: BoxShape.circle,
-                  color: Colors.brown,
-                  gradient: const RadialGradient(
-                    colors: [Colors.brown, Colors.black],
-                  )),
-              child: Center(
-                  child: Text(
-                "$intelligenceTotal",
-                style: const TextStyle(color: Colors.white),
-              )),
-            ),
-            Text("+${(intelligenceTotal - 10) ~/ 2}")
-          ],
-        ),
-        Column(
-          children: [
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                savingThrows.contains("WIS")
-                    ? const Icon(
-                        Icons.circle,
-                        size: 15,
-                      )
-                    : const Icon(
-                        Icons.circle_outlined,
-                        size: 15,
-                      ),
-                const Text("WIS"),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.all(2.5),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 3),
-                  shape: BoxShape.circle,
-                  color: Colors.brown,
-                  gradient: const RadialGradient(
-                    colors: [Colors.brown, Colors.black],
-                  )),
-              child: Center(
-                  child: Text(
-                "$wisdomTotal",
-                style: const TextStyle(color: Colors.white),
-              )),
-            ),
-            Text("+${(wisdomTotal - 10) ~/ 2}")
-          ],
-        ),
-        Column(
-          children: [
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                savingThrows.contains("CHA")
-                    ? const Icon(
-                        Icons.circle,
-                        size: 15,
-                      )
-                    : const Icon(
-                        Icons.circle_outlined,
-                        size: 15,
-                      ),
-                const Text("CHA"),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.all(2.5),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 3),
-                  shape: BoxShape.circle,
-                  color: Colors.brown,
-                  gradient: const RadialGradient(
-                    colors: [Colors.brown, Colors.black],
-                  )),
-              child: Center(
-                  child: Text(
-                "$charismaTotal",
-                style: const TextStyle(color: Colors.white),
-              )),
-            ),
-            Text("+${(charismaTotal - 10) ~/ 2}")
-          ],
-        ),
-      ],
+      ),
     );
   }
 }
