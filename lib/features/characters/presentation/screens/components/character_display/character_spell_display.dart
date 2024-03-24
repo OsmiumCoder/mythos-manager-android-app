e724995b74bf791edd57559ff7a630b8cbf50d57
Jonathon Meney <jonmeney@gmail.com>
2024-03-24 13:57:39 -0300

    finished spell display

--- lib/features/characters/presentation/screens/components/character_display/character_spell_display.dart
+++ lib/features/characters/presentation/screens/components/character_display/character_spell_display.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mythos_manager/features/characters/presentation/controllers/dnd_api_controller.dart';
+import 'package:mythos_manager/features/characters/presentation/screens/components/character_display/spell_card.dart';
 
 class CharacterSpellDisplay extends ConsumerWidget {
   final String className;
@@ -9,12 +10,49 @@ class CharacterSpellDisplay extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return FutureBuilder(
-      future: ref.watch(dndApiController).getSpellsForClass(className),
-      builder:
-          (BuildContext context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-        return const Text("data");
-      },
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            FutureBuilder(
+              future: ref
+                  .watch(dndApiController)
+                  .getSpellsForClass(className.toLowerCase()),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                if (snapshot.hasData) {
+                  List spells = snapshot.data!;
+
+                  if (spells.isEmpty) {
+                    return Center(
+                        child: Text(
+                      "$className has no spells available.",
+                      style: const TextStyle(fontSize: 16),
+                    ));
+                  }
+
+                  return ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: spells.length,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      Map<String, dynamic> spell = spells[index];
+                      return SpellCard(spell: spell);
+                    },
+                  );
+                } else {
+                  return Center(
+                    child: Container(
+                        margin: const EdgeInsets.only(top: 30),
+                        child: const CircularProgressIndicator()),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
